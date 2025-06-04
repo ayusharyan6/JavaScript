b1e5e17f64919adee182d7d68b51ece9ea3de5eb
Ayush Aryan <122227418+ayusharyan6@users.noreply.github.com>
2025-06-04 08:44:19 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer js version
+
+//alert( 3 + 3) // We are using node js not browser
+// browser is the engine of javascript.
+
+console.log( 3 + 3) 
+ console.log("AYUSh");
+
+
+let name = "ayush"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint => basically used to create big websites like trade,etc
+// string => ""
+// boolean => true ya false
+// null => standalone value
+// undefined => value isnt assigned yet
+// symbol => 
+// object
+
+
+console.log(typeof undefined)
+console.log(typeof null) // object
+
