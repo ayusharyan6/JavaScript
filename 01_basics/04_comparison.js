f12cba92a402f8cb20f22e24933cb252399580e7
Ayush Aryan <122227418+ayusharyan6@users.noreply.github.com>
2025-06-04 11:09:05 +0000

    conversion and confusion conversions

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,24 @@
+// console.log(2>1);
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2<=1)
+// console.log(2!=1)
+// console.log(2==1)
+//  basic conversions
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+// always comapare with same data types.
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
+// confusion conversions in most case we will avoid these conversions.
+
+
+console.log("2" === 2);
+// === => not only check but check strictly, like same data types n all.
