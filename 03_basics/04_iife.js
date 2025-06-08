213534565d1668ddfbde66055fd7a2a62cb1fd4d
Ayush Aryan <122227418+ayusharyan6@users.noreply.github.com>
2025-06-08 09:30:17 +0000

    iife done

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,10 @@
+//  IMMEDIATELY Invoked Function Expression  (IIFE) => the function whos execute immediately/
+
+(function coffe (){
+    console.log(`DB CONNECTED`);
+})();   
+//wrapped inside the parenthesis  => also this save from the pollution happening outside the global variable.
+
+(function chai() {
+    console.log(`DB connected`);   
+}) ();
