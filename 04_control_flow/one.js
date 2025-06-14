6ad30629fca60ecd1bb8f7ec7adb247b18d3a3a0
Ayush Aryan <122227418+ayusharyan6@users.noreply.github.com>
2025-06-14 17:01:47 +0000

    conditional ongoing

---
+++ 04_control_flow/one.js
@@ -0,0 +1,18 @@
+//  if statement
+const isUserLoggedIn = true
+const temperature = 41
+
+// if(temperature < 50){
+//     console.log("less than 50");
+// }
+// console.log("temperature is greater than 50");
+
+
+if(temperature < 50){
+    console.log("less than 50");
+}
+else{
+    console.log("temperature is greater than 50");    
+}
+// 
+
