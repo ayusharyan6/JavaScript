7a4da2b87bce535b403ba9c9aa3ebc5fbb8b8950
Ayush Aryan <122227418+ayusharyan6@users.noreply.github.com>
2025-06-14 17:24:20 +0000

    some more ex.

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -21,10 +21,35 @@ if(score > 100){
     console.log(`User power: ${power}`);   
 }
 
-const score = 200;
-if(score > 100){
-    const power = "fly"
-   // console.log(`User power: ${power}`);   
-}
-console.log(`User power: ${power}`);   
+// const score = 200;
+// if(score > 100){
+//     const power = "fly"
+//    // console.log(`User power: ${power}`);   
+// }
+// console.log(`User power: ${power}`);   
 // not executed because u r defining const outside the scope fucntion if u use var it will be executed because var is globally accesseed.
+
+// shorthand notation
+// const balance = 2000
+// if(balance > 400) console.log("test"); // u can also write as this but use curly braces intead of tbis i prefer.
+
+const balance2 = 2000
+if(balance2 <= 30000){
+    console.log("Balanced is left");
+}else{
+    console.log("not sufficient amount");
+    
+}
+
+// multiple checking
+const balance = 5000
+if(balance < 500){
+    console.log("less than");
+}else if(balance < 750){
+    console.log("less than 750");
+}else if(balance < 900){
+    console.log("less than 900");
+    
+}else{
+    console.log("less than 12200");
+}
