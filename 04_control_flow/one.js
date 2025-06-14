10002eaf333962c57d0f5913f5764671f45a635e
Ayush Aryan <122227418+ayusharyan6@users.noreply.github.com>
2025-06-14 17:13:15 +0000

    some more examples

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -14,5 +14,17 @@ if(temperature < 50){
 else{
     console.log("temperature is greater than 50");    
 }
-// 
 
+const score = 200;
+if(score > 100){
+    const power = "fly"
+    console.log(`User power: ${power}`);   
+}
+
+const score = 200;
+if(score > 100){
+    const power = "fly"
+   // console.log(`User power: ${power}`);   
+}
+console.log(`User power: ${power}`);   
+// not executed because u r defining const outside the scope fucntion if u use var it will be executed because var is globally accesseed.
