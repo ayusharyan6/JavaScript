b0117b59198d8d8ff424ab535aa2faf5eb7a331f
Ayush Aryan <122227418+ayusharyan6@users.noreply.github.com>
2025-06-14 17:39:56 +0000

    conditonal going

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -72,3 +72,49 @@ if(onlinePayment || cash){
     console.log("not allowed");
     
 }
+
+// switch statement
+
+const month = 4
+
+switch(month){
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("june");
+        break;
+    case 7:
+        console.log("july");
+        break;
+    case 8:
+        console.log("aug");
+        break;
+    case 9:
+        console.log("sept");
+        break;
+    case 10:
+        console.log("oct");
+        break;
+    case 11:
+        console.log("nov");
+        break;
+    case 12:
+        console.log("dec");
+        break;
+        case defalut:
+            console.log("invalid input");
+            break
+}
